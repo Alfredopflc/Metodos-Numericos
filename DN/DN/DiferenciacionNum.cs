c630c4bcf07f5c79d9edd992fd8a563124367f6e
JOSE VENEGAS <42922208+Alfredopflc@users.noreply.github.com>
2019-01-27 23:58:00 -0800

    Add files via upload

---
+++ DN/DN/DiferenciacionNum.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DN
+{
+    class DiferenciacionNum
+    {
+        public double Sumai (double[] valor, int num, double Xi)
+        {
+            double sumai = 0;
+
+            for (int i = 0; i <= num; i++)
+            {
+                sumai = sumai + (valor[i] * (Math.Pow(Xi, i)));
+            }
+
+            return sumai;
+        }
+
+        public void Imprimir(double[] valor, int num)
+        {
+            Console.WriteLine("\nFuncion: ");
+
+            for (int j = num; j >= 0; j--)
+            {
+                Console.Write(" + ( {0}X^{1} )", valor[j], j);
+            }
+        }
+
+        public void DelantePriDer1(double[] valor,double Xi, double h, double XiMs1, int num)
+        {
+            double resultado = 0;
+
+            resultado = (Sumai(valor, num, XiMs1) - Sumai(valor, num, Xi)) / h;
+            Imprimir(valor, num);
+            
+            Console.WriteLine("\nTu resultado es: {0:0.00000} ", resultado);
+        }
+
+        public void DelantePriDer2(double[] valor, double Xi, double h, double XiMs1, double XiMs2, int num)
+        {
+            double resultado = 0;
+
+            resultado = ((-1 * Sumai(valor,num,XiMs2)) + (4 * Sumai(valor, num, XiMs1)) - (3 * Sumai(valor, num, Xi))) / (2 * h);
+
+            Imprimir(valor, num);
+
+            Console.WriteLine("\nTu resultado es: {0:0.00000} ", resultado);
+
+        }
+
+        public void DelanteSegDer1(double[] valor, double Xi, double h, double XiMs1, double XiMs2, int num)
+        {
+            double resultado = 0;
+           
+            resultado = ((Sumai(valor, num, XiMs2)) - (2 * Sumai(valor, num, XiMs1)) + (Sumai(valor, num, Xi))) / (Math.Pow(h,2));
+
+            Imprimir(valor, num);
+
+            Console.WriteLine("\nTu resultado es: {0:0.00000} ", resultado);
+        }
+
+        public void DelanteSegDer2(double[] valor, double Xi, double h, double XiMs1, double XiMs2, double XiMs3, int num)
+        {
+            double resultado = 0;           
+
+            resultado = ((-1 * Sumai(valor, num, XiMs3)) + (4 * Sumai(valor, num, XiMs2)) - (5 * Sumai(valor, num, XiMs1)) + (2 * Sumai(valor, num, Xi))) / (Math.Pow(h, 2));
+
+            Imprimir(valor, num);
+
+            Console.WriteLine("\nTu resultado es: {0:0.00000} ", resultado);
+        }
+
+        public void AtrasPriDer1(double[] valor, double Xi, double h, double XiMn1, int num)
+        {
+            double resultado = 0;
+
+            resultado = (Sumai(valor, num, Xi) - Sumai(valor, num, XiMn1)) / h;
+
+            Imprimir(valor, num);
+
+            Console.WriteLine("\nTu resultado es: {0:0.00000} ", resultado);
+        }
+
+        public void AtrasPriDer2(double[] valor, double Xi, double h, double XiMn1, double XiMn2, int num)
+        {
+            double resultado = 0;
+
+            resultado = ((3 * Sumai(valor, num, Xi)) - (4 * Sumai(valor, num, XiMn1)) + Sumai(valor, num, XiMn2)) / (2 * h);
+
+            Imprimir(valor, num);
+
+            Console.WriteLine("\nTu resultado es: {0:0.00000} ", resultado);
+        }
+
+        public void AtrasSegDer1(double[] valor, double Xi, double h, double XiMn1, double XiMn2, int num)
+        {
+            double resultado = 0;    
+
+            resultado = ((Sumai(valor, num, Xi)) - (2 * Sumai(valor, num, XiMn1)) + (Sumai(valor, num, XiMn2))) / (Math.Pow(h, 2));
+
+            Imprimir(valor, num);
+
+            Console.WriteLine("\nTu resultado es: {0:0.00000} ", resultado);
+        }
+
+        public void AtrasSegDer2(double[] valor, double Xi, double h, double XiMn1, double XiMn2, double XiMn3, int num)
+        {
+            double resultado = 0;
+
+            resultado = ((2 * Sumai(valor, num, Xi)) - (5 * Sumai(valor, num, XiMn1)) + (4 * Sumai(valor, num, XiMn2)) - (Sumai(valor, num, XiMn3))) / Math.Pow(h, 2);
+
+            Imprimir(valor, num);
+
+            Console.WriteLine("\nTu resultado es: {0:0.00000} ", resultado);
+        }
+
+        public void CentradaPriDer1(double[] valor,double h, double XiMs1, double XiMn1, int num)
+        {
+            double resultado = 0;
+
+            resultado = ((Sumai(valor, num, XiMs1) - Sumai(valor, num, XiMn1))) / (2 * h);
+
+            Imprimir(valor, num);
+
+            Console.WriteLine("\nTu resultado es: {0:0.00000} ", resultado);
+        }
+
+        public void CentradaPriDer2(double[] valor, double h,double XiMs2, double XiMs1, double XiMn1, double XiMn2, int num)
+        {
+            double resultado = 0;
+
+            resultado = ((-1 * Sumai(valor, num, XiMs2)) + (8 * Sumai(valor, num, XiMs1)) - (8 * Sumai(valor, num, XiMn1)) + (Sumai(valor, num, XiMn2))) / (12 * h);
+
+            Imprimir(valor, num);
+
+            Console.WriteLine("\nTu resultado es: {0:0.00000} ", resultado);
+        }
+
+        public void CentradaSegDer1(double[] valor, double h, double XiMs1, double Xi, double XiMn1, int num)
+        {
+            double resultado = 0;
+
+            resultado = ((Sumai(valor, num, XiMs1)) - (2 * Sumai(valor, num, Xi)) + (Sumai(valor, num, XiMn1))) / (Math.Pow(h, 2));
+
+            Imprimir(valor, num);
+
+            Console.WriteLine("\nTu resultado es: {0:0.00000} ", resultado);
+        }
+
+        public void CentradaSegDer2(double[] valor, double h, double XiMs2, double XiMs1, double Xi, double XiMn1, double XiMn2, int num)
+        {
+            double resultado = 0;
+
+            resultado = ((-1 * Sumai(valor, num, XiMs2)) + (16 * Sumai(valor, num, XiMs1)) - (30 * Sumai(valor, num, Xi)) + (16 * Sumai(valor, num, XiMn1)) - (Sumai(valor, num, XiMn2))) / (12 * Math.Pow(h, 2));
+
+            Imprimir(valor, num);
+
+            Console.WriteLine("\nTu resultado es: {0:0.00000} ", resultado);
+        }
+
+
+    }
+}
---
+++ DN/DN/Program.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DiferenciacionNum Dif = new DiferenciacionNum();
+
+            int num = 0, tipodif = 0, tipoderivada = 0, error = 0, opc = 0;
+            double Xi = 0, h = 0, XiMn3 = 0, XiMn2 = 0, XiMn1 = 0, XiMs1 = 0, XiMs2 = 0, XiMs3;
+
+            do
+            {
+
+                Console.Write("Ingrese el exponente mas grande de la funcion: ");
+                num = int.Parse(Console.ReadLine());
+
+                double[] Valores = new double[num + 1];
+
+                for (int i = num; i >= 0; i--)
+                {
+                    Console.Write("Ingrese constante del exponente {0}: ", i);
+                    Valores[i] = double.Parse(Console.ReadLine());
+                }
+
+                Console.Write("\nIngrese valor de Xi: ");
+                Xi = double.Parse(Console.ReadLine());
+
+                Console.Write("\nIngrese valor de h: ");
+                h = double.Parse(Console.ReadLine());
+
+                XiMn3 = Xi - (3 * h);
+                XiMn2 = Xi - (2 * h);
+                XiMn1 = Xi - h;
+                XiMs1 = Xi + h;
+                XiMs2 = Xi + (2 * h);
+                XiMs3 = Xi + (3 * h);
+
+                Console.Write("\nSeleccione que derivada desea: \n1 = Primera derivada \n2 = Segunda Derivada \nR = ");
+                tipoderivada = int.Parse(Console.ReadLine());
+
+                Console.Write("\nSeleccione Tipo de diferenciacion: \n1 = Hacia Delante \n2 = Hacia Atras \n3 = Centrada \nR = ");
+                tipodif = int.Parse(Console.ReadLine());
+
+
+                // Hacia Delante
+                if (tipodif == 1 && tipoderivada == 1)
+                {
+                    Console.Write("\nIngrese Error deseado: \n1 = O(h) \n2 = O(h^2) \nR = ");
+                    error = int.Parse(Console.ReadLine());
+
+                    if (error == 1)
+                        Dif.DelantePriDer1(Valores, Xi, h, XiMs1, num);
+
+                    else if (error == 2)
+                        Dif.DelantePriDer2(Valores, Xi, h, XiMs1, XiMs2, num);
+
+                    else
+                        Console.WriteLine("ERROR ");
+
+                }
+
+                else if (tipodif == 1 && tipoderivada == 2)
+                {
+                    Console.Write("\nIngrese Error deseado: \n1 = O(h) \n2 = O(h^2) \nR =");
+                    error = int.Parse(Console.ReadLine());
+
+                    if (error == 1)
+                        Dif.DelanteSegDer1(Valores, Xi, h, XiMs1, XiMs2, num);
+
+                    else if (error == 2)
+                        Dif.DelanteSegDer2(Valores, Xi, h, XiMs1, XiMs2, XiMs3, num);
+
+                    else
+                        Console.WriteLine("ERROR ");
+                }
+
+                //HaciaAtras
+                else if (tipodif == 2 && tipoderivada == 1)
+                {
+                    Console.Write("\nIngrese Error deseado: \n1 = O(h) \n2 = O(h^2) \nR =");
+                    error = int.Parse(Console.ReadLine());
+
+                    if (error == 1)
+                        Dif.AtrasPriDer1(Valores, Xi, h, XiMn1, num);
+
+                    else if (error == 2)
+                        Dif.AtrasPriDer2(Valores, Xi, h, XiMn1, XiMn2, num);
+
+                    else
+                        Console.WriteLine("ERROR ");
+
+                }
+
+                else if (tipodif == 2 && tipoderivada == 2)
+                {
+                    Console.Write("\nIngrese Error deseado: \n1 = O(h) \n2 = O(h^2) \nR =");
+                    error = int.Parse(Console.ReadLine());
+
+                    if (error == 1)
+                        Dif.AtrasSegDer1(Valores, Xi, h, XiMn1, XiMn2, num);
+
+                    else if (error == 2)
+                        Dif.AtrasSegDer2(Valores, Xi, h, XiMn1, XiMn2, XiMn3, num);
+
+                    else
+                        Console.WriteLine("ERROR ");
+                }
+
+                //Centrada
+                else if (tipodif == 3 && tipoderivada == 1)
+                {
+                    Console.Write("\nIngrese Error deseado: \n1 = O(h^2) \n2 = O(h^4) \nR =");
+                    error = int.Parse(Console.ReadLine());
+
+                    if (error == 1)
+                        Dif.CentradaPriDer1(Valores, h, XiMs1, XiMn1, num);
+
+                    else if (error == 2)
+                        Dif.CentradaPriDer2(Valores, h, XiMs2, XiMs1, XiMn1, XiMn2, num);
+
+                    else
+                        Console.WriteLine("ERROR ");
+
+                }
+
+                else if (tipodif == 3 && tipoderivada == 2)
+                {
+                    Console.Write("\nIngrese Error deseado: \n1 = O(h^2) \n2 = O(h^4) \nR =");
+                    error = int.Parse(Console.ReadLine());
+
+                    if (error == 1)
+                        Dif.CentradaSegDer1(Valores, h, XiMs1, Xi, XiMn1, num);
+
+                    else if (error == 2)
+                        Dif.CentradaSegDer2(Valores, h, XiMs2, XiMs1, Xi, XiMn1, XiMn2, num);
+
+                    else
+                        Console.WriteLine("ERROR ");
+                }
+
+                else
+                    Console.WriteLine("ERROR ");
+
+                Console.WriteLine("\nDesea volver a iniciar el programa? \n1 = Si \n2 = No");
+                opc = int.Parse(Console.ReadLine());
+
+            } while (opc == 1);
+
+            Console.ReadKey();
+        }
+    }
+}
